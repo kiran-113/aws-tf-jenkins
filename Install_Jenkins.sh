966b2d13ec32c794acf3f558d21d27e2d6e33106
harsha <nakirikantikiran@outlook.com>
2023-09-01 20:23:47 +0530

    updated read me

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -1,12 +1,15 @@
-# This script it to install jenkins, terraform, git , java in AMAZON LINUX 2023
+## This script it to install jenkins, terraform, git , java in AMAZON LINUX 2023
+## Copy the script and psate at userdata in AWS EC2 AMAZON LINUX
+
 #!/bin/bash
-sudo su
+sudo yum install java -y
+sudo yum install maven -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-#NOTE:  you can find the default jenkins admin password at cat  /var/lib/jenkins/secrets/initialAdminPassword
-sudo yum install java -y
+#NOTE:  you can find the default jenkins admin password at---- cat  /var/lib/jenkins/secrets/initialAdminPassword
 sudo yum install git -y
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # aws-tf-jenkins
+
+Install jenkins in aws ec2 in AMAZON LINUX ONLY using the script in install_jenkins.sh
+
+## change the git path in jenkinsfile @line no 18
+This is parameterized jenkins pipeline
+
+after initilizeing , cliek on build and select build with parameter
