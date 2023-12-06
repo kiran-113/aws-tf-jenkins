959cc0e3c23de06a9753cf3eb9021ec9a15bd395
harsha <nakirikantikiran@outlook.com>
2023-12-06 10:57:48 +0530

    refactored code

--- output.tf
+++ output.tf
@@ -1,9 +1,5 @@
-output "public_ip" {
-  value       = aws_instance.jenkins_instance.public_ip
-  description = "Public IP Address of EC2 instance"
-}
+output "Jenkins_web" {
 
-output "instance_id" {
-  value       = aws_instance.jenkins_instance.id
-  description = "Instance ID"
+  value = "http://${aws_instance.jenkins_instance.public_ip}:8080"
+
 }
