5db97d519b80759747cc600300a4e6a6b11d5c2c
harsha <nakirikantikiran@outlook.com>
2024-01-23 19:40:11 +0530

    updated tag

--- variables.tf
+++ variables.tf
@@ -32,5 +32,5 @@ variable "instance_type" {
 variable "name_tag" {
   type        = string
   description = "Name of the EC2 instance"
-  default     = "Jenkins-Instance"
+  default     = "Jenkins-Instance-tf"
 }
