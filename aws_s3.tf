b3a8fb9d50333d2bb5b3c4e6e79bdee777386ffa
harsha <nakirikantikiran@outlook.com>
2024-03-26 10:24:08 +0530

    added new s3

--- aws_s3.tf
+++ aws_s3.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "devops_rtt" {
     Environment = "dev"
   }
 }
+
+resource "aws_s3_bucket" "devops_rtt2" {
+  bucket        = "devops-rtt-test-bu-46584"
+  force_destroy = true
+
+  tags = {
+    Name        = "My_bucket"
+    Environment = "dev"
+  }
+}
